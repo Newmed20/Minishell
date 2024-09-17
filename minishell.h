20a76188bd75ef2c6a8a20622815a7cb7663641f
Abdelali <abde69436@gmail.com>
2024-09-17 10:20:16 +0100

    main

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mjadid <mjadid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:47:07 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/09/17 08:16:33 by mjadid           ###   ########.fr       */
+/*   Updated: 2024/09/17 09:47:36 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,13 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <readline/readline.h>
+# include <readline/history.h>
 # include <stdbool.h>
 # include "libft/libft.h"
+
+#define	PROMPT "minishell <$"
+
+typedef struct	s_list_ t_list_;
 
 enum e_state
 {
@@ -53,14 +58,69 @@ typedef struct s_token
 	struct s_token	*prev;
 }	t_token;
 
-typedef struct s_lst
+typedef struct s_tkn_lst
 {
 	t_token	*head;
 	t_token	*tail;
-}	t_lst;
+}	t_tkn_lst;
+
+typedef struct	s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
+typedef struct	s_command
+{
+	char				*command; // The command name (e.g., "echo", "ls")
+	char				*full_path; // The full path to the command (e.g., "/bin/echo", "/bin
+	char				**args; // Array of arguments including the command
+	int					arg_count; // Number of arguments
+	t_list_				*input_files; // For input redirection (<)
+	t_list_				*oa_files; // For output redirection (>)
+	t_list_				*heredoc_delimiters; // For heredoc (<<)
+	t_list_				*heredoc_content; // Content of heredoc
+	int					pipe_out; // 1 if this command pipes to next, 0 otherwise
+	struct s_command	*next; // Pointer to next command in pipeline
+}	t_command;
+
+struct s_list_
+{
+	void	*content;
+	int		type;
+	struct s_list_	*next;
+};
+
+typedef struct	s_data
+{
+	char		*prompt;
+	t_command	*cmd;
+	t_env		*env_copy;
+	t_tkn_lst	*lex;
+}	t_data;
 
 t_token	*skip_spaces(t_token *el, int flg);
-int		syntax_error(t_lst *lst);
+
+/* ------------------- errors ------------------- */
+
+int		syntax_error(t_tkn_lst	*lst);
 int		print_error(void);
+
+/* ------------------- lexer ------------------- */
+
+t_tkn_lst	*lexer(char *line);
+
+/* ------------------- utils ------------------- */
+
+char	**ft_strdup_2d(char **_2d);
+
+/* ------------------- expander ------------------- */
+
+t_data	*get_env_cpy(t_data *data, char **env);
+
+
+
+void	print_token(t_tkn_lst *lst); // !!!!!!!!!!!!!!!
 
 #endif
