ba82597da9c546370c72724b624d07d884097ad5
Abdelali <abde69436@gmail.com>
2024-09-17 10:20:47 +0100

    utils

---
+++ utils/utils1.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils1.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 22:38:10 by abmahfou          #+#    #+#             */
+/*   Updated: 2024/09/16 22:38:10 by abmahfou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	free_2d(char **str, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+char	**ft_strdup_2d(char **_2d)
+{
+	int		i;
+	int		len;
+	char	**res;
+
+	i = 0;
+	len = 0;
+	while (_2d[len])
+		len++;
+	res = malloc((len + 1) * sizeof(char *));
+	if (!res)
+		return (NULL);
+	while (_2d[i])
+	{
+		res[i] = ft_strdup(_2d[i]);
+		if (!res[i])
+		{
+			free_2d(res, i);
+			return (NULL);
+		}
+		i++;
+	}
+	res[i] = NULL;
+	return (res);
+}
