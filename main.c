96dfbb727f103cf5ebb0e091a29a01fbcf6bfe05
Abdelali <abde69436@gmail.com>
2024-09-21 19:09:48 +0100

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 11:59:13 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/09/18 20:55:14 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/09/21 16:33:13 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,37 @@
 
 int	init_data(int ac, char **av, char **env, t_data *data)
 {
-	(void)av;
 	char	**env_tmp;
 
+	(void)av;
 	env_tmp = ft_strdup_2d(env);
 	if (ac != 1)
 	{
-		printf("minihell doesn't accept any argument\n");
-		return (1);
+		printf("minishell doesn't accept any argument\n");
+		return (EXIT_FAILURE);
 	}
 	data->cmd = NULL;
 	data->env_copy = NULL;
-	data->lex = NULL;
+	data->lexer = NULL;
 	data->prompt = NULL;
 	get_env_cpy(data, env_tmp);
-	return (0);
+	return (EXIT_SUCCESS);
 }
 
 int	init_prompt(t_data *data)
 {
 	char	*input;
-	
+
 	input = readline(PROMPT);
 	if (!input)
 	{
 		printf("exit\n");
-		return (1);
+		return (EXIT_FAILURE);
 	}
 	data->prompt = input;
 	add_history(input);
-	return (0);
+	ft_parser(data);
+	return (EXIT_SUCCESS);
 }
 
 int	main(int ac, char **av, char **env)
@@ -51,12 +52,11 @@ int	main(int ac, char **av, char **env)
 	t_data	data;
 
 	if (init_data(ac, av, env, &data) == 1)
-		return (0);
+		return (EXIT_SUCCESS);
 	while (1)
 	{
 		if (init_prompt(&data) == 1)
 			break ;
 	}
-	
-	return (0);
+	return (EXIT_SUCCESS);
 }
