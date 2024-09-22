a49177e27d14a0fa853c7346ca21ba76978726c0
Abdelali <abde69436@gmail.com>
2024-09-22 15:12:18 +0100

    add parser

--- parser/parser.c
+++ parser/parser.c
@@ -6,16 +6,17 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 22:48:22 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/09/19 22:48:22 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/09/22 10:49:55 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void    ft_parser(t_data *data)
+void	ft_parser(t_data *data)
 {
-    if (!input_error(data))
-    {
-        
-    }
+	data->lexer = lexer(data->prompt);
+	if (!syntax_error(data->lexer))
+	{
+		ft_expand(data);
+	}
 }
