bca232bd3f3a1d9318557e2b5d8a0dcdbc85fe88
Abdelali <abde69436@gmail.com>
2024-09-24 00:02:34 +0100

    utils

--- utils/utils1.c
+++ utils/utils1.c
@@ -12,6 +12,19 @@
 
 #include "../minishell.h"
 
+void	free_split(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
 void	free_2d(char **str, int n)
 {
 	int	i;
