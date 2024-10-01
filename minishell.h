9bcc139489683c7fa289c7d3d547db4ce2341447
Abdelali <abde69436@gmail.com>
2024-10-01 13:05:43 +0100

    edit header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:47:07 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/09/24 14:52:37 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/10/01 10:44:28 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,35 +114,49 @@ typedef struct	s_data
 	t_tkn_lst	*lexer;
 }	t_data;
 
-t_token	*skip_spaces(t_token *el, int flg);
 
 /* ------------------- errors ------------------- */
 
-int		syntax_error(t_tkn_lst	*lst);
-int		print_error(int errror);
+int	syntax_error(t_tkn_lst	*lst);
+int	print_error(int errror);
 
 /* ------------------- lexer ------------------- */
 
-t_tkn_lst *lexer(char *line);
+t_tkn_lst	*lexer(char *line);
+void		free_tkn_lst(t_tkn_lst *lst);
+t_token		*skip_spaces(t_token *el, int flg);
 
 /* ------------------- utils ------------------- */
 
 char	**ft_strdup_2d(char **_2d);
 void	free_2d(char **str, int n);
 void	free_split(char **arr);
+bool	ft_isspace(char c);
 
 /* ------------------- expander ------------------- */
 
-t_data 		*get_env_cpy(t_data *data, char **env);
+t_data		*get_env_cpy(t_data *data, char **env);
 t_var_name	*ft_expand(t_data *data);
 char		*get_digit(char c, int pos, t_var_name *name, t_data *data);
 char		*get_var_value(t_env *env, char *key);
 char		*get_after(char *str, t_var_name *var_name);
 char		*get_full(char *prompt, t_var_name *var);
 
+/* ------------------- parser ------------------- */
+
 void	ft_parser(t_data *data);
-int		ft_is_command(t_data *data, char *cmd);
+int		ft_is_command(t_data *data, t_command *command, char *cmd);
+t_redir	*init_list(void);
+void	append_to_list(t_redir **lst, t_redir *new);
+void	free_command(t_command **cmd);
+void	handle_redirections_heredoc(t_token *token, t_command *cmd);
+t_redir	*create_redir(t_token *token);
+int		is_redir(t_token *token);
+void	handle_heredoc(t_token *token, t_command *cmd);
+void	lst_add_back(t_command **cmds, t_command *cmd);
 
 void	print_token(t_tkn_lst *lst); // !!!!!!!!!!!!!!!
+char	*print_type(enum e_type type); // !!!!!!!!!!!!!!
+
 
 #endif
