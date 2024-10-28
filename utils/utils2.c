359fad2abe217dd701a43a2ee938bedc42c5039e
Abdelali <abde69436@gmail.com>
2024-10-28 16:58:45 +0100

    utils

---
+++ utils/utils2.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 13:43:59 by abmahfou          #+#    #+#             */
+/*   Updated: 2024/10/28 13:44:09 by abmahfou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+t_token	*skip_spaces(t_token *el, int flg)
+{
+	while (el && el->type == WHITE_SPACE)
+	{
+		if (flg && el->next)
+			el = el->next;
+		else if (!flg && el->prev)
+			el = el->prev;
+		else
+			break ;
+	}
+	return (el);
+}
