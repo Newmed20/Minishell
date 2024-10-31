7dbb53de0781892efd2153c22e10a3131b25d692
Abdelali <abde69436@gmail.com>
2024-10-31 21:55:13 +0100

    utils

--- utils/utils2.c
+++ utils/utils2.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:43:59 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/10/28 13:44:09 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/10/31 21:38:00 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,21 @@ t_token	*skip_spaces(t_token *el, int flg)
 			break ;
 	}
 	return (el);
+}
+
+int	is_special(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '#' || s[i] == '@' || s[i] == '%' || s[i] == '!'
+			|| s[i] == '^' || s[i] == '&' || s[i] == '*' || s[i] == '~'
+			|| s[i] == '-' || s[i] == '+' || s[i] == '.' || s[i] == ';'
+			|| s[i] == '}' || s[i] == '{' || s[i] == '?' || s[i] == '$' || s[i] == '=')
+			return (1);
+		i++;
+	}
+	return (0);
 }
