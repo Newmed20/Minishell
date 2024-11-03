6e1bfbcb788b262082a02152ae4ae5f1e441df36
Mohammed Jadid <hamoda.style.000@gmail.com>
2024-11-03 21:11:20 +0100

    edited ridirections

--- execution/redirections.c
+++ execution/redirections.c
@@ -6,34 +6,73 @@
 /*   By: mjadid <mjadid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 07:19:00 by mjadid            #+#    #+#             */
-/*   Updated: 2024/10/29 07:19:28 by mjadid           ###   ########.fr       */
+/*   Updated: 2024/11/03 15:57:02 by mjadid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 
+
+void	dup_close(int fd)
+{
+	if (dup2(fd, STDOUT_FILENO) == -1)
+	{
+		perror("dup2");
+		exit(1);
+	}
+	close(fd);
+}
+
+int	ft_output(t_redir *output_files)
+{
+	int	fd = -1;
+
+	while (output_files)
+	{
+		if (fd != -1)
+			close(fd);
+		if (output_files->type == REDIR_OUT)
+			fd = open(output_files->content, O_WRONLY
+					| O_CREAT | O_TRUNC, 0644);
+		else
+			fd = open(output_files->content, O_WRONLY 
+					| O_CREAT | O_APPEND, 0644);
+		if (fd == -1)
+		{
+			perror("open");
+			exit(1);
+		}
+		output_files = output_files->next;
+	}
+	dup_close(fd);
+	return (0);
+}
+
+int	ft_input(t_redir *input_file)
+{
+	int	fd = -1;
+
+	while (input_file)
+	{
+		if (fd != -1)
+			close(fd);
+		fd = open(input_file->content, O_RDONLY);
+		if (fd == -1)
+		{
+			perror("open");
+			exit(1);
+		}
+		input_file = input_file->next;
+	}
+	dup_close(fd);
+	return (0);
+}
+
 void ft_redirection(t_command *cmd)
 {
-    int fd;
-    t_redir *redir;
-    redir = cmd->input_files;
-    while(redir)
-    {
-        fd = open(redir->content, O_RDONLY);
-        dup2(fd, 0);
-        close(fd);
-        redir = redir->next;
-    }
-    redir = cmd->oa_files;
-    while(redir)
-    {
-        if(redir->type == REDIR_OUT )
-            fd = open(redir->content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        else if(redir->type == APPEND)
-            fd = open(redir->content, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        dup2(fd, 1);
-        close(fd);
-        redir = redir->next;
-    }
+	if (cmd->input_files)
+		ft_input(cmd->input_files);
+	if (cmd->oa_files)
+		ft_output(cmd->oa_files);
 }
