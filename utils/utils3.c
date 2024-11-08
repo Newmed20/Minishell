e4cbeadb07772cca5d8d655f9af7ef77b75974f1
Abdelali <abde69436@gmail.com>
2024-11-08 18:25:14 +0100

    utils

--- utils/utils3.c
+++ utils/utils3.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:52:42 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/11/07 21:17:59 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/11/08 18:01:32 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ void	update_old_pwd(t_data *data, char *path)
 	while (tmp)
 	{
 		if (!ft_strncmp(tmp->key, "OLDPWD", 7))
+		{
+			free(tmp->value);
 			tmp->value = ft_strdup(path);
+		}
 		tmp = tmp->next;
 	}
 }
@@ -33,7 +36,10 @@ void	update_pwd(t_data *data, char *path)
 	while (tmp)
 	{
 		if (!ft_strncmp(tmp->key, "PWD", 4))
+		{
+			free(tmp->value);
 			tmp->value = ft_strdup(path);
+		}
 		tmp = tmp->next;
 	}
 }
