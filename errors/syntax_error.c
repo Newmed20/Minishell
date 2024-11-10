531741d63e035ea1779966514a8b97dc278afa07
Abdelali <abde69436@gmail.com>
2024-11-10 16:28:24 +0100

    errors

--- errors/syntax_error.c
+++ errors/syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 16:07:54 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/11/04 22:21:25 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/11/10 15:13:45 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,17 +60,17 @@ int	syntax_error(t_tkn_lst *lst)
 			|| token->type == REDIR_OUT || token->type == HERE_DOC)
 		{
 			if (_redir_error(token))
-				return (exit_status = 258, print_error(1));
+				return (g_exit_status = 258, print_error(1));
 		}
 		else if (token->type == PIPE_LINE)
 		{
 			if (_pipe_error(token))
-				return (exit_status = 258, print_error(2));
+				return (g_exit_status = 258, print_error(2));
 		}
 		else if (token->type == S_QUOTE || token->type == D_QUOTE)
 		{
 			if (!_unclosed_quotes(&token, token->type))
-				return (exit_status = 258, EXIT_FAILURE);
+				return (g_exit_status = 258, EXIT_FAILURE);
 		}
 		token = token->next;
 	}
